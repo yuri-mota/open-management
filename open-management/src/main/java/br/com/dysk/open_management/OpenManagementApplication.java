9d0a4ed581c991245ecaa6708d5130c43492585a
Yuri Mota <yuri.mota@outlook.com.br>
2024-09-06 14:05:49 -0300

    Initial commit with java backend project

---
+++ open-management/src/main/java/br/com/dysk/open_management/OpenManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.dysk.open_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenManagementApplication.class, args);
+	}
+
+}
---
+++ open-management/src/test/java/br/com/dysk/open_management/OpenManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dysk.open_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
